ac3b2dd710c589be1f4ee91c320ff84b94309577
markryu <atienza.new@gmail.com>
2019-02-06 11:39:47 +0800

    modified print and docstring

--- problem_2/even_fibonacci_numbers.py
+++ problem_2/even_fibonacci_numbers.py
@@ -1,7 +1,8 @@
 
 def main():
     '''gets the fibonacci sequence whose values do not exceed four million, find 
-    the sum of the even-valued terms.'''
+    the sum of the even-valued terms.
+    This function assumes it started at 0'''
     preceeding_number = 0
     current_number = 1
     even_list = list()
@@ -11,7 +12,7 @@ def main():
         current_number = sum_of_two
         if current_number % 2 == 0:
             even_list.append(current_number)
-    print (sum(even_list))
+    print (f"The total sum of all even numbers from the fibonacci sequence less than 4 million is {sum(even_list)}")
     return sum(even_list)
         
 
